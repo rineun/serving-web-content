8e04081c0d17a41c491c3afd6ff48a7a32ba902f
rineun <sy880808@gmail.com>
2021-05-17 16:04:11 +0900

    login security

---
+++ src/main/java/com/example/servingwebcontent/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.servingwebcontent.repository;
+import com.example.servingwebcontent.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/example/servingwebcontent/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.servingwebcontent.service;
+
+import com.example.servingwebcontent.domain.Role;
+import com.example.servingwebcontent.domain.User;
+import com.example.servingwebcontent.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user){
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(encodePassword);
+        user.setEnabled(true);
+
+        Role role = new Role();
+        role.setId(1L);
+        user.getRoles().add(role);
+
+        return userRepository.save(user);
+    }
+}
